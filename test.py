58bab3cd3753232847ac9c124eaeee92a09de082
Boris <bili1@student.monash.edu>
2017-09-09 18:06:07 +1000

    Stuff

--- test.py
+++ test.py
@@ -1,11 +1,4 @@
-vaccines = {'australia': ["1a", 2, 3, 4], 'new zealand': [1, 2, 3, 4]}
-list = {}
-for i in range(0, 3):
-    details = []
+import requests
 
-    for j in range(0, 4):
-        details.append(j)
-
-    list[i] = details
-
-print(list)
+r = requests.post('https://api.github.com/events', data={'1': '2'})
+print(r.text)
